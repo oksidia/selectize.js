14ba639c438216dfa55099b4837649704419de5d
Miikka Virtanen <dragory.up@gmail.com>
2015-10-30 11:17:21 +0200

    fix: add missing KEY_SPACE constant

--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,7 @@ var IS_MAC        = /Mac/.test(navigator.userAgent);
 var KEY_A         = 65;
 var KEY_COMMA     = 188;
 var KEY_RETURN    = 13;
+var KEY_SPACE     = 32;
 var KEY_ESC       = 27;
 var KEY_LEFT      = 37;
 var KEY_UP        = 38;
